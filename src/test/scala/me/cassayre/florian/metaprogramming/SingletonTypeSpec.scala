fe43ed586fb1001c2e13368f4dd0b6df68f86ac1
Florian Cassayre <florian.cassayre@gmail.com>
2025-10-26 11:45:30 +0100

    Use constValue over summonValue when possible

--- src/test/scala/me/cassayre/florian/metaprogramming/SingletonTypeSpec.scala
+++ src/test/scala/me/cassayre/florian/metaprogramming/SingletonTypeSpec.scala
@@ -1,10 +1,11 @@
 package me.cassayre.florian.metaprogramming
 
 import me.cassayre.florian.metaprogramming.SingletonType.*
-import me.cassayre.florian.metaprogramming.test.Assert.*
 import org.scalatest.funsuite.AnyFunSuite
+
+import scala.compiletime.constValue
 
 class SingletonTypeSpec extends AnyFunSuite:
 
   test("summons the correct GCD result"):
-    assert(6 === summonValue[Gcd[48, 18]])
+    assert(6 === constValue[Gcd[48, 18]])
